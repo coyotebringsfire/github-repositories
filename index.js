7709f304be81a204f9a37f1a685b7f8b15f4b59e
CoyoteBringsFire <aumkara@gmail.com>
2015-08-21 11:36:12 -0700

    added options.url to ctor args + tests

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function GithubRepo() {
+var util 			= require('util'),
+	EventEmitter 	= require('events').EventEmitter;
+
+function GithubRepo(options) {
+	if( options && options.url ) {
+		this.url = options.url;
+	} else {
+		this.url = "https://api.github.com/repositories";
+	}
 	util.inherits(this, EventEmitter);
 }
+
+GithubRepo.prototype.start = function() {
+	// body...
+};
 
 module.exports = GithubRepo;
--- test/index.js
+++ test/index.js
@@ -1,8 +1,35 @@
 var should 	= require('should');
 
 describe("github-repositories", function libSuite() {
-	var gr 	= require('../');
+	var GR 	= require('../');
 	it("should return a constructor", function doIt() {
-		gr.should.be.type('function');
+		GR.should.be.type('function');
+	});
+	describe("GR class", function grSuite() {
+		it("should use the default github api URL", function doIt(done) {
+			var gr = new GR();
+			gr.url.should.equal("https://api.github.com/repositories");
+			done();
+		});
+		it("should use the custom url passed in the options object passed as the first argument", function doIt(done) {
+			should.fail();
+		});
+		describe("#stop", function stopMethod() {
+			it("should stop requesting repositories", function doIt(done) {
+				should.fail();
+			});
+		});
+		describe("#start", function startMethod() {
+			it("should start requesting repositories", function doIt(done) {
+				should.fail();
+			});
+		});
+		it("should emit a repo event for each repo returned", function doIt(done) {
+			var gr = new GR();
+			gr.on('repo', function(evt) {
+				gr.stop();
+				done();
+			});
+		});
 	});
 });
