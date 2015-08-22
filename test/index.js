ba909646e53577a71912070f136ab216d1497171
CoyoteBringsFire <aumkara@gmail.com>
2015-08-22 10:36:06 -0700

    refactored test order

--- test/index.js
+++ test/index.js
@@ -38,45 +38,6 @@ describe("github-repositories", function libSuite() {
 			gr.url.should.equal("TESTURL");
 			done();
 		});
-		describe("#stop", function stopMethod() {
-			it("should stop requesting repositories", function doIt(done) {
-				var gr = new GR();
-				gr.start();
-				setTimeout( function onTimeout() {
-					var numRepos = gr.repos.length;
-					gr.stop();
-					gr.stopped.should.be.ok;
-					numRepos.should.equal( gr.repos.length);
-					done();
-				}, 500);
-			});
-			it("should set stopped variable to true even if the scraper never started", function doIt(done) {
-				var gr = new GR();
-				gr.stop();
-				gr.stopped.should.be.ok;
-				done();
-			});
-		});
-		describe("#start", function startMethod() {
-			it("should start requesting repositories", function doIt(done) {
-				var gr = new GR();
-				gr.start();
-				gr.on('repo', function onRepo(evt) {
-					gr.repos.length.should.be.greaterThan(0);
-					if( !gr.stopped ) {
-						gr.stop();
-						done();
-					}
-				});
-			});
-			it("should set stopped variable to false", function doIt(done) {
-				var gr = new GR();
-				gr.start();
-				gr.stopped.should.not.be.ok;
-				gr.stop();
-				done();
-			});
-		});
 		it("should restart requests after the ratelimit has refreshed", function doIt(done) {
 			this.timeout(10000);
 			var gr = new GR();
@@ -123,6 +84,45 @@ describe("github-repositories", function libSuite() {
 				nock.cleanAll();
 				done();
 			}, 900);
+		});
+		describe("#stop", function stopMethod() {
+			it("should stop requesting repositories", function doIt(done) {
+				var gr = new GR();
+				gr.start();
+				setTimeout( function onTimeout() {
+					var numRepos = gr.repos.length;
+					gr.stop();
+					gr.stopped.should.be.ok;
+					numRepos.should.equal( gr.repos.length);
+					done();
+				}, 500);
+			});
+			it("should set stopped variable to true even if the scraper never started", function doIt(done) {
+				var gr = new GR();
+				gr.stop();
+				gr.stopped.should.be.ok;
+				done();
+			});
+		});
+		describe("#start", function startMethod() {
+			it("should start requesting repositories", function doIt(done) {
+				var gr = new GR();
+				gr.start();
+				gr.on('repo', function onRepo(evt) {
+					gr.repos.length.should.be.greaterThan(0);
+					if( !gr.stopped ) {
+						gr.stop();
+						done();
+					}
+				});
+			});
+			it("should set stopped variable to false", function doIt(done) {
+				var gr = new GR();
+				gr.start();
+				gr.stopped.should.not.be.ok;
+				gr.stop();
+				done();
+			});
 		});
 	});
 });
